a41f166f7686b21300e367dae0b91888a320a079
lmh <lmhlmhtju@163.com>
2020-12-08 19:00:29 +0800

    实现Reactor官网上的API练手项目

--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -27,6 +27,7 @@ public class Part11BlockingToReactive {
 
 	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
+
 		return Flux.defer(()->Flux.fromIterable(repository.findAll())).subscribeOn(Schedulers.elastic());
 	}
 
